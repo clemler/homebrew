99579729f8e6843f6df0b7a510eb376e5980f3a8
Chris Lemler <chris@lemler.org>
2015-07-12 22:11:41 -0700

    Basic setup() complete

--- Arduino/dc-motor-hbridge/dc-motor-hbridge.ino
+++ Arduino/dc-motor-hbridge/dc-motor-hbridge.ino
@@ -1,14 +1,32 @@
 //dc-motor-hbridge.ino
+//
+// This sketch uses an SN754410 H-Bridge to control the direction of a DC
+// motor. There is a digital switch attached to Pin 2 (toggelDirectionPin),
+// which is used to toggle the direction of the motor. In future versions
+// there will be a second motor, hence the naming conventions (motor1...).
 
-int led = 13;
+const int toggleDirectionPin = 2;       // Toggle based on switch input
+const int motor1hbridge2Pin = 3;        // H-Bridge pin 2 (1A)
+const int motor1hbridge7Pin = 4;        // H-Bridge pin 7 (2A)
+const int motor1EnablePin = 9;          // H-Bridge pin 1 (1,2EN)
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
+    // Set the toggle switch as an input
+    pinMode(toggleDirectionPin, INPUT);
+
+    // Set all other pins as outputs
+    pinMode(motor1hbridge2Pin, OUTPUT);
+    pinMode(motor1hbridge7Pin, OUTPUT);
+    pinMode(motor1EnablePin, OUTPUT);
+
+    // Set the motor1EnablePin to HIGH so the motor will turn on
+    digitalWrite(motor1EnablePin, HIGH);
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
     digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(100);               // wait for a second
     digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
