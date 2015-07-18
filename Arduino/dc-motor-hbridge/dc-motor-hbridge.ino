0ce3081a1149ca9f87cec0f45ea6481b475f4626
clemler <chris@lemler.org>
2015-07-18 11:53:30 -0700

    Added logic to drive a second motor

--- Arduino/dc-motor-hbridge/dc-motor-hbridge.ino
+++ Arduino/dc-motor-hbridge/dc-motor-hbridge.ino
@@ -1,14 +1,26 @@
 //dc-motor-hbridge.ino
 //
-// This sketch uses an SN754410 H-Bridge to control the direction of a DC
-// motor. There is a digital switch attached to Pin 2 (toggelDirectionPin),
-// which is used to toggle the direction of the motor. In future versions
-// there will be a second motor, hence the naming conventions (motor1...).
+// This sketch uses an SN754410 H-Bridge to control the direction of two DC
+// motors. There is a digital switch attached to Pin 2 (toggelDirectionPin),
+// which is used to toggle the direction of the motor. There are two motors
+// in my prototype, but you can easily comment out Motor2 if you are only
+// using a single motor.
+//
+// @Todo: I still need to enable PWM on the motor1EnablePin to control the motor
+// speed. In future versions
 
 const int toggleDirectionPin = 2;       // Toggle based on switch input
+const int motor1EnablePin = 9;          // H-Bridge pin 1 (1,2EN)
+const int motor2EnablePin = 10;          // H-Bridge pin 9 (3,4EN)
+
+// Motor 1 (Right Motor)
 const int motor1hbridge2Pin = 3;        // H-Bridge pin 2 (1A)
 const int motor1hbridge7Pin = 4;        // H-Bridge pin 7 (2A)
-const int motor1EnablePin = 9;          // H-Bridge pin 1 (1,2EN)
+
+// Motor 2 (Right Motor)
+const int motor2hbridge10Pin = 6;        // H-Bridge pin 10 (3A)
+const int motor2hbridge15Pin = 7;        // H-Bridge pin 15 (4A)
+
 
 
 void setup() {
@@ -19,9 +31,13 @@ void setup() {
     pinMode(motor1hbridge2Pin, OUTPUT);
     pinMode(motor1hbridge7Pin, OUTPUT);
     pinMode(motor1EnablePin, OUTPUT);
+    pinMode(motor2hbridge10Pin, OUTPUT);
+    pinMode(motor2hbridge15Pin, OUTPUT);
+    pinMode(motor2EnablePin, OUTPUT);
 
-    // Set the motor1EnablePin to HIGH so the motor will turn on
+    // Set the motor*EnablePins to HIGH so the motors will turn on
     digitalWrite(motor1EnablePin, HIGH);
+    digitalWrite(motor2EnablePin, HIGH);
 }
 
 
@@ -31,9 +47,13 @@ void loop() {
     if ( digitalRead(toggleDirectionPin) == HIGH) {
         digitalWrite(motor1hbridge2Pin, LOW);   // Set leg 1 of the H-Bridge to LOW
         digitalWrite(motor1hbridge7Pin, HIGH);   // Set leg 2 of the H-Bridge to HIGH
+        digitalWrite(motor2hbridge10Pin, LOW);   // Set leg 1 of the H-Bridge to LOW
+        digitalWrite(motor2hbridge15Pin, HIGH);   // Set leg 2 of the H-Bridge to HIGH
     }
     else {  // Switch LOW, turn the other direction
         digitalWrite(motor1hbridge2Pin, HIGH);   // Set leg 1 of the H-Bridge to HIGH
         digitalWrite(motor1hbridge7Pin, LOW);   // Set leg 2 of the H-Bridge to LOW
+        digitalWrite(motor2hbridge10Pin, HIGH);   // Set leg 1 of the H-Bridge to HIGH
+        digitalWrite(motor2hbridge15Pin, LOW);   // Set leg 2 of the H-Bridge to LOW
     }
 }
