52af4ca0c77646eca676db96d89a525e979c72cf
clemler <chris@lemler.org>
2015-07-19 08:41:15 -0700

    Completed logic so that the button toggles between off, fwd, and rev

--- Arduino/dc-motor-hbridge/dc-motor-hbridge.ino
+++ Arduino/dc-motor-hbridge/dc-motor-hbridge.ino
@@ -9,7 +9,7 @@
 // @Todo: I still need to enable PWM on the motor1EnablePin to control the motor
 // speed. In future versions
 
-const int toggleDirectionPin = 2;       // Toggle based on switch input
+const int togglePin = 2;                // Toggle based on switch input
 const int motor1EnablePin = 9;          // H-Bridge pin 1 (1,2EN)
 const int motor2EnablePin = 10;          // H-Bridge pin 9 (3,4EN)
 
@@ -21,11 +21,21 @@ const int motor1hbridge7Pin = 4;        // H-Bridge pin 7 (2A)
 const int motor2hbridge10Pin = 6;        // H-Bridge pin 10 (3A)
 const int motor2hbridge15Pin = 7;        // H-Bridge pin 15 (4A)
 
+int motorState = 0;                      // 0 = off, 1 = fwd, 2 = rev
+int buttonState;                         // the current reading from the input pin
+int lastButtonState = LOW;               // the previous reading from the input pin
 
+// the following variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup() {
+    // Setup so we can log messages
+    Serial.begin(9600);
+
     // Set the toggle switch as an input
-    pinMode(toggleDirectionPin, INPUT);
+    pinMode(togglePin, INPUT);
 
     // Set all other pins as outputs
     pinMode(motor1hbridge2Pin, OUTPUT);
@@ -35,25 +45,78 @@ void setup() {
     pinMode(motor2hbridge15Pin, OUTPUT);
     pinMode(motor2EnablePin, OUTPUT);
 
-    // Set the motor*EnablePins to HIGH so the motors will turn on
+    // Set the motor*EnablePins to LOW so they are initially off
+    digitalWrite(motor1EnablePin, LOW);
+    digitalWrite(motor2EnablePin, LOW);
+}
+
+// Set the motor*EnablePins to HIGH so the motors will turn on
+void turnMotorsOn() {
     digitalWrite(motor1EnablePin, HIGH);
     digitalWrite(motor2EnablePin, HIGH);
 }
 
+// Set the motor*EnablePins to LOW so the motors will turn off
+void turnMotorsOff() {
+    digitalWrite(motor1EnablePin, LOW);
+    digitalWrite(motor2EnablePin, LOW);
+}
+
 
 void loop() {
-    // If the switch (toggleDirectionPin) is HIGH. the motor will turn
-    // in one direction
-    if ( digitalRead(toggleDirectionPin) == HIGH) {
-        digitalWrite(motor1hbridge2Pin, LOW);   // Set leg 1 of the H-Bridge to LOW
-        digitalWrite(motor1hbridge7Pin, HIGH);   // Set leg 2 of the H-Bridge to HIGH
-        digitalWrite(motor2hbridge10Pin, LOW);   // Set leg 1 of the H-Bridge to LOW
-        digitalWrite(motor2hbridge15Pin, HIGH);   // Set leg 2 of the H-Bridge to HIGH
+    // read the state of the switch into a local variable:
+    int reading = digitalRead(togglePin);
+
+    // check to see if you just pressed the button
+    // (i.e. the input went from LOW to HIGH),  and you've waited
+    // long enough since the last press to ignore any noise:
+
+    // If the switch changed, due to noise or pressing:
+    if (reading != lastButtonState) {
+        // reset the debouncing timer
+        lastDebounceTime = millis();
     }
-    else {  // Switch LOW, turn the other direction
-        digitalWrite(motor1hbridge2Pin, HIGH);   // Set leg 1 of the H-Bridge to HIGH
-        digitalWrite(motor1hbridge7Pin, LOW);   // Set leg 2 of the H-Bridge to LOW
-        digitalWrite(motor2hbridge10Pin, HIGH);   // Set leg 1 of the H-Bridge to HIGH
-        digitalWrite(motor2hbridge15Pin, LOW);   // Set leg 2 of the H-Bridge to LOW
+
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+        // whatever the reading is at, it's been there for longer
+        // than the debounce delay, so take it as the actual current state:
+
+        // if the button state has changed:
+        if (reading != buttonState) {
+            buttonState = reading;
+
+            // only toggle the LED if the new button state is HIGH
+            if (buttonState == HIGH) {
+                motorState = (motorState + 1) % 3;
+                Serial.print("Motor State: ");
+                Serial.println(motorState);
+            }
+        }
     }
+
+    switch (motorState) {
+        case 0:
+            turnMotorsOff();
+            break;
+        case 1:
+            turnMotorsOn();
+            digitalWrite(motor1hbridge2Pin, HIGH);   // Set leg 1 of the H-Bridge to HIGH
+            digitalWrite(motor1hbridge7Pin, LOW);   // Set leg 2 of the H-Bridge to LOW
+            digitalWrite(motor2hbridge10Pin, HIGH);   // Set leg 1 of the H-Bridge to HIGH
+            digitalWrite(motor2hbridge15Pin, LOW);   // Set leg 2 of the H-Bridge to LOW
+            break;
+        case 2:
+            turnMotorsOn();
+            digitalWrite(motor1hbridge2Pin, LOW);   // Set leg 1 of the H-Bridge to LOW
+            digitalWrite(motor1hbridge7Pin, HIGH);   // Set leg 2 of the H-Bridge to HIGH
+            digitalWrite(motor2hbridge10Pin, LOW);   // Set leg 1 of the H-Bridge to LOW
+            digitalWrite(motor2hbridge15Pin, HIGH);   // Set leg 2 of the H-Bridge to HIGH
+            break;
+        default:
+            break;
+    }
+
+    // save the reading.  Next time through the loop,
+    // it'll be the lastButtonState:
+    lastButtonState = reading;
 }
